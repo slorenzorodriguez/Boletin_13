e01bfbf24cafe3760365af613669706151964273
slorenzorodriguez <slorenzorodriguez@nautilus22.danielcastelao.org>
2018-12-12 11:41:38 +0100

    Ejercicio terminado;

---
+++ src/boletin13/Xogador.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package boletin13;
+
+/**
+ *
+ * @author slorenzorodriguez
+ */
+
+  
+    public class Xogador extends SeleccionFutbol {
+
+    private int dorsal;
+    private String demarcacion;
+
+   
+    public Xogador() {
+        super();
+
+    }  
+}
